69b427685ba6eff326a97fa8b0c139d63461305b
Michael Pereira <pereira.m@gmail.com>
2012-03-07 23:02:00 -0500

    Start of the import method

--- src/Pretzel.Logic/Import/WordpressImportSql.cs
+++ src/Pretzel.Logic/Import/WordpressImportSql.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO.Abstractions;
+using System.Text.RegularExpressions;
 
 namespace Pretzel.Logic.Import
 {
-    public class WordpressImportSql
+    public class WordpressImportSql : WordpressImport
     {
         private readonly IFileSystem fileSystem;
         private readonly string pathToSite;
         private readonly string pathToImportFile;
 
         public WordpressImportSql(IFileSystem fileSystem, string pathToSite, string pathToImportFile)
+            : base(fileSystem, pathToSite, pathToImportFile)
         {
+
             this.fileSystem = fileSystem;
             this.pathToSite = pathToSite;
             this.pathToImportFile = pathToImportFile;
@@ -23,12 +26,19 @@ namespace Pretzel.Logic.Import
         {
             int counter = 0;
             string line;
-
-            // Read the file and display it line by line.
+            Regex postRegex = new Regex(@"INSERT INTO `wp_posts` VALUES \((?<postId>\d+), (?<authorId>\d+), '[^']+', '(?<dategmt>[^']+)', '(?<content>.*)', '(?<title>.*)', '.*', 'publish'");
+            
+            IList<Tuple<int, int, WordpressPost>> postList = new List<Tuple<int, int, WordpressPost>>();
             System.IO.StreamReader file = new System.IO.StreamReader(pathToImportFile);
             while ((line = file.ReadLine()) != null)
             {
-                
+                Match match = postRegex.Match(line);
+                if (match.Success)
+                {                    
+                    int postId = int.Parse(match.Groups["postId"].Value);
+                    int authorId = int.Parse(match.Groups["authorId"].Value);
+                    postList.Add(new Tuple<int, int, WordpressPost>(postId, authorId, new WordpressPost()));
+                }
                 counter++;
             }
 
